ba974b0124664787d9739f0dfe63a46017a376a7
pranjalhinge <pranjalhinge4@gmail.com>
2024-07-19 17:35:42 +0530

    add js

--- javascript/navbar&footer.js
+++ javascript/navbar&footer.js
@@ -1,4 +1,20 @@
-function insta(){
-   const instaElement = document.getElementById("socialmedia");
-   const src = "https://www.instagram.com/foodweb_imphal?igsh=bHJ5cGIza2VucWt5"
+function enableDark(){
+   const mainBody = document.getElementById("main-body")
+   mainBody.classList.add("bg-dark")
+   mainBody.classList.remove("bg-light")
+
+   const toggleButton = document.getElementById("btn-toggle")
+   toggleButton.classList.remove("btn-light")
+   toggleButton.classList.add("btn-dark")
+}
+function enableLight(){
+   const mainBody = document.getElementById("main-body")
+   mainBody.classList.add("bg-light")
+   mainBody.classList.remove("bg-dark")
+
+   const toggleButton = document.getElementById("btn-toggle")
+
+
+   toggleButton.classList.add("btn-light")
+   toggleButton.classList.remove("btn-dark")
 }
