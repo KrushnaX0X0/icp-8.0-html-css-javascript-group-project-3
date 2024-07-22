3e6d5dd2d921a29b743107f3d3de7f9c76e80b4d
Harshofft <hdharsh122@gmail.com>
2024-07-22 13:32:24 +0530

    nav bar js change

--- javascript/navbar&footer.js
+++ javascript/navbar&footer.js
@@ -18,3 +18,14 @@ function enableLight(){
    toggleButton.classList.add("btn-light")
    toggleButton.classList.remove("btn-dark")
 }
+const menuButton = document.getElementById("menu")
+const closeButton = document.getElementById("close")
+function menu(hi) {
+   let head = document.getElementById("head")
+   if (hi==1) {
+      head.classList.add("active")
+   }
+   else {
+      head.classList.remove("active")
+   }
+}
