61afa1d6448ea5378c2f7c6dca92148050172a21
ankitakangane <kanganeankita1@gmail.com>
2024-07-23 16:37:34 +0530

    apply some css in cards

---
+++ javascript/blog.js
@@ -0,0 +1,11 @@
+const menuButton = document.getElementById("menu")
+const closeButton = document.getElementById("close")
+function menu(hi) {
+   let head = document.getElementById("head")
+   if (hi==1) {
+      head.classList.add("active")
+   }
+   else {
+      head.classList.remove("active")
+   }
+}
