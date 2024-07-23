912c20fc871f11c88293773bcec8ffbc4875cc83
ankitakangane <kanganeankita1@gmail.com>
2024-07-23 12:08:51 +0530

    java script used for 5th card for buy the product

--- javascript/counter.js
+++ javascript/counter.js
@@ -54,7 +54,20 @@ function handleQuantityChange1st(action){
     quantityElement.innerText = action == 'inc'? ++quantity : --quantity;
     }
 
-
+function handleQuantityChange5th(action){
+        const quantityElement=document.getElementById("quantity-text5th");
+        let quantity =parseInt( quantityElement.innerText);
+       if(quantity == 1 && action == 'dec'){
+        alert("Quantity can not be less then 1");
+       return;
+        }
+       else if(quantity == 6 && action == 'inc'){
+        alert("Quantity can not be greater then 6");
+       return;
+        }
+        quantityElement.innerText = action == 'inc'? ++quantity : --quantity;
+ }
+    
     
 
 
