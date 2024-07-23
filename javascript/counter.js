d3fc421860a3015180b95ce175c08710be4e516f
ankitakangane <kanganeankita1@gmail.com>
2024-07-23 12:10:15 +0530

    java script used for 6th card for buy the product

--- javascript/counter.js
+++ javascript/counter.js
@@ -67,6 +67,21 @@ function handleQuantityChange5th(action){
         }
         quantityElement.innerText = action == 'inc'? ++quantity : --quantity;
  }
+ 
+ function handleQuantityChange6th(action){
+    const quantityElement=document.getElementById("quantity-text6th");
+    let quantity =parseInt( quantityElement.innerText);
+   if(quantity == 1 && action == 'dec'){
+    alert("Quantity can not be less then 1");
+   return;
+    }
+   else if(quantity == 6 && action == 'inc'){
+    alert("Quantity can not be greater then 6");
+   return;
+    }
+    quantityElement.innerText = action == 'inc'? ++quantity : --quantity;
+    }
+
     
     
 
