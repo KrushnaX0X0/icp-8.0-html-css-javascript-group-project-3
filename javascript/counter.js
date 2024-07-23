c52efcea8c02838bd9c15cf849d1f44cd122e4cd
ankitakangane <kanganeankita1@gmail.com>
2024-07-23 12:02:31 +0530

    java script used for 2nd card for buy the product

--- javascript/counter.js
+++ javascript/counter.js
@@ -10,7 +10,21 @@ function handleQuantityChange1st(action){
    return;
     }
     quantityElement.innerText = action == 'inc'? ++quantity : --quantity;
+ }
 
-
+ function handleQuantityChange2nd(action){
+    const quantityElement=document.getElementById("quantity-text2nd");
+    let quantity =parseInt( quantityElement.innerText);
+   if(quantity == 1 && action == 'dec'){
+    alert("Quantity can not be less then 1");
+   return;
     }
+   else if(quantity == 6 && action == 'inc'){
+    alert("Quantity can not be greater then 6");
+   return;
+    }
+    quantityElement.innerText = action == 'inc'? ++quantity : --quantity;
+    }
+
+
 
