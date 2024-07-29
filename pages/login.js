a8ab247e9518509f816c243e24d7815499020c32
pranjalhinge <pranjalhinge4@gmail.com>
2024-07-29 17:19:56 +0530

    add js

--- pages/login.js
+++ pages/login.js
@@ -24,3 +24,7 @@ document.getElementById('login-form').addEventListener('submit', function(event)
         errorMessage.textContent = 'Invalid username or password.';
     }
 });
+
+     type = "module" src = "https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js">
+     src = "./../javascript/navbar&footer.js">
+     nomodule src = "https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js">
