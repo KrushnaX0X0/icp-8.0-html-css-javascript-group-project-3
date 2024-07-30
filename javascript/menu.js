572c91747f23da12e180c673055b1ab33c260d77
rutuja441 <rutujasabale441@gmail.com>
2024-07-30 18:10:54 +0530

    init commit

--- javascript/menu.js
+++ javascript/menu.js
@@ -23,33 +23,6 @@ function toRemove(active) {
     
 }
 
-// $(document).ready(function () {
-//     let timeSpent = 0;
-
-//     function updateTime() {
-//         timeSpent++;
-//         $('#productivity-counter').text(Time spent on this site: ${timeSpent} seconds);
-//     }
-
-//     setInterval(updateTime, 1000);
-
-//     $('.filter').click(function () {
-//         const filter = $(this).data('filter');
-//         $('.menu-item').hide();
-//         $(filter).show();
-//         $('.filter-active').removeClass('filter-active');
-//         $(this).addClass('filter-active');
-//     });
-
-//     const swiper = new Swiper('.swiper-container', {
-//         pagination: {
-//             el: '.swiper-pagination',
-//         },
-//         loop: true,
-//     });
-// });
-
-
 const menuButton = document.getElementById("menu")
 const closeButton = document.getElementById("close")
 function menu(hi) {
